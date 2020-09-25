71608cad10e5df6ea7bf1062c04d3cf01f31f945
Jared Cline <jared.cline@gmail.com>
2020-09-25 18:42:39 -0400

    added config, started a WordList class

--- passphrase.py
+++ passphrase.py
@@ -1,7 +1,32 @@
-wordListFn = ""
-wordList = Null
+import configparser as cfp
+
+
+class WordList:
+    def dump(self):
+        for w in self.list:
+            print(w)
+        print(self.wordCt)
+
+
+    def __init__(self):
+        self.list = [] # maybe a set???
+        self.wordCt = 0
+
+
+def configure():
+    global wordList
+    conf = cfp.ConfigParser()
+    conf.read("config.ini")
+
+
+# def genWord(wl):
+#     return = ""
+
 
 def main():
-    print("helloWorld")
+    configure()
+    wordList = WordList()
+    wordList.dump()
+
 
 main()
