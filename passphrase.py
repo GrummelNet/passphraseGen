29dbf136a8b914a14a571c315c30e0900e614f49
Jared Cline <jared.cline@gmail.com>
2020-09-28 21:18:41 -0400

    added a test wordlist and expanded the WordList class some more

--- passphrase.py
+++ passphrase.py
@@ -6,17 +6,31 @@ class WordList:
         for w in self.list:
             print(w)
         print(self.wordCt)
+        print(self.minWordLen)
+        print(self.maxWordLen)
 
 
-    def __init__(self):
+    # constructor helper function
+    def loadList(self, listFn):
+        corpus = open(listFn)
+        for word in corpus:
+            self.list.append(word.strip())
+            self.wordCt += 1
+
+
+    def __init__(self, conf):
         self.list = [] # maybe a set???
         self.wordCt = 0
+        self.minWordLen = conf['DEFAULT']['minWordLen']
+        self.maxWordLen = conf['DEFAULT']['maxWordLen']
+        self.loadList(conf['TESTING']['test1'])
 
 
 def configure():
-    global wordList
     conf = cfp.ConfigParser()
     conf.read("config.ini")
+    rv = WordList(conf)
+    return rv
 
 
 # def genWord(wl):
@@ -24,8 +38,7 @@ def configure():
 
 
 def main():
-    configure()
-    wordList = WordList()
+    wordList = configure()
     wordList.dump()
 
 
